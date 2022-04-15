50f768706bc8fddfbf7da6af559a71f4f37acfaf
Victor Bruno <103613078+bruninblue@users.noreply.github.com>
2022-04-15 14:01:23 -0300

    Delete ex03.py

--- ex03.py
+++
@@ -1,8 +0,0 @@
-horaInicio = int(input("Digite o horário de início do jogo de xadrez: "))
-horaTerminio = int(input("Digite o horário de términio do jogo de xadrez: "))
-
-# print((24-horaInicio)+horaTerminio)
-# print(horaTerminio-horaInicio)
-
-print((24-16)+4)
-print(22-7)
